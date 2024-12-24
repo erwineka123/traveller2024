f1c4bfabf2aff5623712e89ebc8131e9d920ab01
erwineka123 <ekaerwin09@gmail.com>
2024-12-24 17:51:32 +0700

    baruuuu

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
+            server: {
+                host: true, // Agar dapat diakses oleh Vercel
+            },
         }),
     ],
 });
